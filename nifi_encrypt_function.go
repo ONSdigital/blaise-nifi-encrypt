8ff0a9e17af440875a55ad6debf917be4e507757
Nik Webb <nik.webb@ons.gov.uk>
2021-03-29 14:00:17 +0100

    Added data delivery status for instaging, encrypted and error

--- nifi_encrypt_function.go
+++ nifi_encrypt_function.go
@@ -2,8 +2,12 @@ package blaise_nifi_encrypt
 
 import (
 	"context"
+	"os"
+
+	"github.com/rs/zerolog/log"
 
 	"github.com/ONSDigital/blaise-nifi-encrypt/pkg"
+	"github.com/ONSDigital/blaise-nifi-encrypt/pkg/datadeliverystatus"
 	"github.com/ONSDigital/blaise-nifi-encrypt/pkg/models"
 	"github.com/ONSDigital/blaise-nifi-encrypt/pkg/util"
 )
@@ -11,5 +15,14 @@ import (
 // handles event from item arriving in the nifi staging bucket
 func NiFiEncryptFunction(ctx context.Context, e models.GCSEvent) error {
 	util.ConfigureLogging()
-	return pkg.HandleEncryptionRequest(ctx, e.Name, e.Bucket)
+	dataDeliveryStatusClient := datadeliverystatus.Client{
+		Config: &datadeliverystatus.Config{
+			BaseURL: os.Getenv("DDS_URL"),
+		},
+	}
+	_, err := dataDeliveryStatusClient.Update(e.Name, "in_staging")
+	if err != nil {
+		log.Error().Msgf("Updating data delivery status to 'in_staging' failed: %s", err.Error())
+	}
+	return pkg.HandleEncryptionRequest(ctx, e.Name, e.Bucket, dataDeliveryStatusClient)
 }
