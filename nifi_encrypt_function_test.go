3709cd96cd0630284d153ad5607d4fce77196727
kristian4res <57638182+Enigma-cloud@users.noreply.github.com>
2023-09-01 13:15:44 +0100

    test: add env var tests

--- nifi_encrypt_function_test.go
+++ nifi_encrypt_function_test.go
@@ -2,11 +2,11 @@ package blaise_nifi_encrypt
 
 import (
 	"context"
+	"net/http"
 	"reflect"
 	"testing"
 
 	"cloud.google.com/go/functions/metadata"
-	http_mocks "github.com/ONSDigital/blaise-nifi-encrypt/nifi_encrypt_function_mocks"
 	"github.com/ONSDigital/blaise-nifi-encrypt/pkg/datadeliverystatus"
 	"github.com/ONSDigital/blaise-nifi-encrypt/pkg/models"
 )
@@ -28,13 +28,19 @@ func TestNiFiEncryptFunction(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "Client id environment variable is not set up",
+			name:    "CLIENT_ID environment variable is not set up",
 			ctx:     metadata.NewContext(context.Background(), meta),
 			e:       event,
 			wantErr: true,
 		},
 		{
-			name:    "Client id environment variable is set with wrong value",
+			name:    "CLIENT_ID environment variable is set with wrong value",
+			ctx:     metadata.NewContext(context.Background(), meta),
+			e:       event,
+			wantErr: true,
+		},
+		{
+			name:    "CLIENT_ID environment variable is an empty string",
 			ctx:     metadata.NewContext(context.Background(), meta),
 			e:       event,
 			wantErr: true,
@@ -43,11 +49,13 @@ func TestNiFiEncryptFunction(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.name == "Client id environment variable is set with wrong value" {
-				{
-					t.Setenv("CLIENT_ID", "dummy")
-				}
+			if tt.name == "CLIENT_ID environment variable is set with wrong value" {
+				t.Setenv("CLIENT_ID", "dummy")
 			}
+			if tt.name == "CLIENT_ID environment variable is an empty string" {
+				t.Setenv("CLIENT_ID", "")
+			}
+
 			err := NiFiEncryptFunction(tt.ctx, tt.e)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("NiFiEncryptFunction() error = %v, wantErr %v", err, tt.wantErr)
@@ -60,20 +68,44 @@ func TestNiFiEncryptFunction(t *testing.T) {
 func Test_createDataDeliveryStatusClient(t *testing.T) {
 	tests := []struct {
 		name    string
-		client  *HTTPClient
+		client  *http.Client
 		want    datadeliverystatus.Client
 		wantErr bool
 	}{
-		// TODO: Add test cases.
 		{
-			name:    "DDS URL is not set up",
-			client:  &http_mocks.NewHTTPClient(),
+			name:    "DDS_URL environment variable isn't set up",
+			client:  &http.Client{},
 			want:    datadeliverystatus.Client{},
 			wantErr: true,
+		},
+		{
+			name:    "DDS_URL environment variable is an empty string",
+			client:  &http.Client{},
+			want:    datadeliverystatus.Client{},
+			wantErr: true,
+		},
+		{
+			name:   "DDS_URL environment variable is set up",
+			client: &http.Client{},
+			want: datadeliverystatus.Client{
+				Config: &datadeliverystatus.Config{
+					BaseURL: "www.blaise.com",
+				},
+				HTTP: &http.Client{},
+			},
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "DDS_URL environment variable is an empty string" {
+				t.Setenv("DDS_URL", "")
+			}
+
+			if tt.name == "DDS_URL environment variable is set up" {
+				t.Setenv("DDS_URL", "www.blaise.com")
+			}
+
 			got, err := createDataDeliveryStatusClient(tt.client)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("createDataDeliveryStatusClient() error = %v, wantErr %v", err, tt.wantErr)
@@ -84,8 +116,4 @@ func Test_createDataDeliveryStatusClient(t *testing.T) {
 			}
 		})
 	}
-}
-
-func NewHTTPClient() {
-	panic("unimplemented")
 }
