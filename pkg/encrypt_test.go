5d55a9ce3978a42e2802c21f48975cf8bf1168fc
kristian4res <57638182+Enigma-cloud@users.noreply.github.com>
2023-09-01 13:16:32 +0100

    test: add env var tests

---
+++ pkg/encrypt_test.go
@@ -0,0 +1,100 @@
+package pkg
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/ONSDigital/blaise-nifi-encrypt/pkg/models"
+)
+
+func Test_loadConfig(t *testing.T) {
+	type args struct {
+		name     string
+		location string
+	}
+	tests := []struct {
+		name         string
+		args         args
+		want         models.Encrypt
+		wantErr      bool
+		desiredError string
+	}{
+		// TODO: Add test cases.\
+		{
+			name: "ENCRYPTION_DESTINATION environment variable is not set",
+			args: args{
+				name:     "test.txt",
+				location: "/home/user",
+			},
+			want:         models.Encrypt{},
+			wantErr:      true,
+			desiredError: "the ENCRYPTION_DESTINATION environment variable has not been set",
+		},
+		{
+			name: "ENCRYPTION_DESTINATION environment variable is an empty string",
+			args: args{
+				name:     "test.txt",
+				location: "/home/user",
+			},
+			want:         models.Encrypt{},
+			wantErr:      true,
+			desiredError: "the ENCRYPTION_DESTINATION environment variable is an empty string",
+		},
+		{
+			name: "PUBLIC_KEY environment variable is not set",
+			args: args{
+				name:     "test.txt",
+				location: "/home/user",
+			},
+			want:    models.Encrypt{},
+			wantErr: true,
+		},
+		{
+			name: "PUBLIC_KEY environment variable is an empty string",
+			args: args{
+				name:     "test.txt",
+				location: "/home/user",
+			},
+			want:    models.Encrypt{},
+			wantErr: true,
+		},
+		{
+			name: "ENCRYPTION_DESTINATION and PUBLIC_KEY environment variables are set",
+			args: args{
+				name:     "test.txt",
+				location: "/home/user",
+			},
+			want: models.Encrypt{
+				KeyFile:               "dummy",
+				FileName:              "test.txt",
+				Location:              "/home/user",
+				EncryptionDestination: "/tmp/encrypted",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "ENCRYPTION_DESTINATION environment variable is an empty string" {
+				t.Setenv("ENCRYPTION_DESTINATION", "")
+			}
+			if tt.name == "PUBLIC_KEY environment variable is an empty string" {
+				t.Setenv("ENCRYPTION_DESTINATION", "/tmp/encrypted")
+				t.Setenv("PUBLIC_KEY", "")
+			}
+			if tt.name == "ENCRYPTION_DESTINATION and PUBLIC_KEY environment variables are set" {
+				t.Setenv("ENCRYPTION_DESTINATION", "/tmp/encrypted")
+				t.Setenv("PUBLIC_KEY", "dummy")
+			}
+
+			got, err := loadConfig(tt.args.name, tt.args.location)
+			if ((err != nil) != tt.wantErr) && (err.Error() == tt.desiredError) {
+				t.Errorf("loadConfig() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("loadConfig() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
